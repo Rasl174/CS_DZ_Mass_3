0fe03fc75496f1152f90fd6b16543ab28cba572f
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-13 15:43:43 +0500

    Изменил

--- CS_DZ_Mass_3/Program.cs
+++ CS_DZ_Mass_3/Program.cs
@@ -16,33 +16,25 @@ namespace CS_DZ_Mass_3
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                array[i] = random.Next(0, 21);
+                array[i] = random.Next(0, 31);
                 Console.Write(array[i] + " ");
+            }
 
-                if ((i == array.GetLength(0) - 1) && (array[0] > array[1]))
+            if (array[0] > array[1])
+            {
+                localMaximum += array[0] + " ";
+            }
+
+            for (int j = 1; j < array.GetLength(0) ; j++)
+            {
+                if ((j == array.GetLength(0) - 1) && (array[array.GetLength(0) - 1] > array[array.GetLength(0) - 2]))
                 {
-                    localMaximum += array[0] + " ";
+                    localMaximum += array[array.GetLength(0) - 1];
                 }
-
-                if (i == array.GetLength(0) - 1)
+                if (array[j] > array[j - 1] && array[j] > array[j + 1])
                 {
-                    for (int j = 1; j < array.GetLength(0) - 1; j++)
-                    {
-
-                        if (array[j] > array[j + 1] && array[j] > array[j - 1])
-                        {
-                            localMaximum += array[j] + " ";
-                        }
-
-                    }
-
+                    localMaximum += array[j] + " ";
                 }
-
-                if ((i == array.GetLength(0) - 1) && (array[array.GetLength(0) - 1] > array[array.GetLength(0) - 2]))
-                {
-                    localMaximum += "" + array[array.GetLength(0) - 1];
-                }
-
             }
             Console.WriteLine("\nЛокальные максимумы: " + localMaximum);
         }
